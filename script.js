c4c629ecca7f4e37b3e85cc4de99e1411db835a6
Hunter Ball <hunterball@email.arizona.edu>
2021-11-12 14:38:43 -0700

    Changed to line graph

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 import { initializeApp } from 'firebase/app';
 import { } from "firebase/database";
 
@@ -18,39 +18,32 @@ const app = initializeApp(firebaseConfig);
 
 // Get a reference to the database service
 const database = getDatabase(app);
+*/
 
 var canvas = document.getElementById("graph");
 var chart;
 function getGraph() {
     canvas.width  = window.innerWidth/2;
-    var xyValues = [
-        {x:50, y:7},
-        {x:60, y:8},
-        {x:70, y:8},
-        {x:80, y:9},
-        {x:90, y:9},
-        {x:100, y:9},
-        {x:110, y:10},
-        {x:120, y:11},
-        {x:130, y:14},
-        {x:140, y:14},
-        {x:150, y:15}
-      ];
+    var xValues = [50,60,70,80,90,100,110,120,130,140,150];
+    var yValues = [7,8,8,9,9,9,10,11,14,14,15];
       
-      chart = new Chart("graph", {
-        type: "scatter",
+    chart = new Chart("graph", {
+        type: "line",
         data: {
+          labels: xValues,
           datasets: [{
-            pointRadius: 4,
-            pointBackgroundColor: "rgba(0,0,255,1)",
-            data: xyValues
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "rgba(0,0,255,1.0)",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: yValues
           }]
         },
-        options:{
-            title: {
-                display: true,
-                text: "Example Graph"
-            }
+        options: {
+          legend: {display: false},
+          scales: {
+            yAxes: [{ticks: {min: 6, max:16}}],
+          }
         }
       });
 }
