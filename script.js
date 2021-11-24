abc333cb8651f8c1c03049223640008b034b2057
Hunter Ball <hunterball@email.arizona.edu>
2021-11-23 17:56:40 -0700

    Start database integration

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-/*
 import { initializeApp } from 'firebase/app';
-import { } from "firebase/database";
+import { } from 'firebase/database';
 
 // Set the configuration for your app
 // TODO: Replace with your project's config object
 const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "projectId.firebaseapp.com",
-  // For databases not in the us-central1 location, databaseURL will be of the
-  // form https://[databaseName].[region].firebasedatabase.app.
-  // For example, https://your-database-123.europe-west1.firebasedatabase.app
-  databaseURL: "https://databaseName.firebaseio.com",
-  storageBucket: "bucket.appspot.com"
+  apiKey: "AIzaSyA2zF2IufwdTP59fIV709tzKd0Cx4IGp-E",
+  authDomain: "thisgraphis.firebaseapp.com",
+  databaseURL: "https://thisgraphis-default-rtdb.firebaseio.com",
+  projectId: "thisgraphis",
+  storageBucket: "thisgraphis.appspot.com",
+  messagingSenderId: "442066372663",
+  appId: "1:442066372663:web:aeb3ed81833b0494e897b2",
+  measurementId: "G-QKYE2MK2F5"
 };
 
 const app = initializeApp(firebaseConfig);
 
 // Get a reference to the database service
 const database = getDatabase(app);
-*/
 
 var canvas = document.getElementById("graph");
 var chart;
