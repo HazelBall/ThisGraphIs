5259810d549f922802c6eea0ced5de0522dada53
Hunter Ball <hunterball@email.arizona.edu>
2021-12-06 13:35:00 -0700

    Finished website

--- createGraph.js
+++ createGraph.js
@@ -1,7 +1,93 @@
-function exportGraph(db, title, type, xAxis, yAxis, xData, yData) {
-    // TODO
+function createGraph() {
+    remove();
+    enableReactions();
+    localStorage.setItem("currentGraph", "New Graph");
+    let title = prompt("Type a title for your new graph!", "Number of Monkeys in California.");
+    let yLabel = prompt("TYpe what your graph measures:", "Cost, Population, etc.")
+    var xLabel;
+    var xData = [], yData = [];
+    var dataPoints = Math.floor(Math.random() * 100);
+    if(Math.floor(Math.random() * 10) <= 5) {
+        xLabel = "Number";
+        for(var i = 1; i <= dataPoints; i ++) {
+            xData.push(i);
+        }
+
+    } else {
+        xLabel = "Year";
+        var year = Math.floor(Math.random()*100) + 1950;
+        var n = Math.floor(Math.random() * 5);
+        for(var i = 0; i < dataPoints; i ++) {
+            xData.push(year);
+            year += n;
+        }
+    }
+    var dataLast = Math.floor(Math.random() * 250);
+    for(var i = 0; i < dataPoints; i ++) {
+        var noiseDif = Math.floor(Math.random() * 30) - 15;
+        yData.push(dataLast + noiseDif);
+        dataLast += noiseDif;
+    }
+
+    var ctx = document.getElementById("graph");
+    chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: xData,
+            datasets: [{
+                label: yLabel,
+                fill: true,
+                lineTension: 0,
+                data: yData
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            tooltips: {
+                enabled: false
+            },
+            title: {
+                display: true,
+                text: title,
+                fontSize: 24
+            },
+            scales : {
+                xAxes: [{
+                    scaleLabel: {
+                        labelString: xLabel,
+                        display: true
+                    }
+                }],
+                yAxes: [{
+                    scaleLabel: {
+                        labelString: yLabel,
+                        display: true,
+                    }
+                }]
+            }
+        }
+    });
+    exportGraph(title, "line", xLabel, yLabel, xData, yData);
 }
 
-function importFromDoc(doc) {
-
+function exportGraph(title, type, xLabel, yLabel, xData, yData) {
+    collection = db.collection("graphs");
+    collection.add({
+        title: title,
+        type: type,
+        xLabel: xLabel,
+        yLabel: yLabel,
+        xData: xData,
+        yData: yData,
+        reactHappy:0,
+        reactSad:0,
+        reactAngry:0,
+        reactConfused:0,
+        reactLaugh:0,
+        reports:0
+    }).then(function(docRef) {
+        localStorage.setItem("currentGraph", docRef.id);
+    });
 }
--- script.js
+++ script.js
@@ -5,68 +5,69 @@ function getGraph() {
   canvas.width  = window.innerWidth/2;
   graphID = null;
   data = null;
+  graphFound = false;
   db.collection("graphs").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
       if(localStorage.getItem(doc.id) == null || localStorage.getItem(doc.id) == false) {
         graphID = doc.id;
         data = doc.data();
+        graphFound = true;
         localStorage.setItem("currentGraph", doc.id);
-        // Uncomment this when the full app is ready/creating graphs is complete
-        //localStorage.setItem(graphID, true);
       }
     });
-      
-    var ctx = document.getElementById("graph");
-    chart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: data.xData,
-        datasets: [{
-          label: data.yLabel,
-          fill: true,
-          lineTension: 0,
-          data: data.yData
-        }]
-      },
-      options: {
-        legend: {
-          display: false
-        },
-        tooltips: {
-          enabled: false
-        },
-        title: {
-          display: true,
-          text: data.title,
-          fontSize: 24
-        },
-        scales : {
-          xAxes: [{
-            scaleLabel: {
-              labelString: data.xLabel,
-              display: true
-            }
-          }],
-          yAxes: [{
-            scaleLabel: {
-              labelString: data.yLabel,
-              display: true,
-            }
+
+    if(!graphFound) {
+      disableReactions();
+      alert("No more graphs could be retrieved. Create a new one!");
+    } else {
+      var ctx = document.getElementById("graph");
+      chart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: data.xData,
+          datasets: [{
+            label: data.yLabel,
+            fill: true,
+            lineTension: 0,
+            data: data.yData
           }]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          tooltips: {
+            enabled: false
+          },
+          title: {
+            display: true,
+            text: data.title,
+            fontSize: 24
+          },
+          scales : {
+            xAxes: [{
+              scaleLabel: {
+                labelString: data.xLabel,
+                display: true
+              }
+            }],
+            yAxes: [{
+              scaleLabel: {
+                labelString: data.yLabel,
+                display: true,
+              }
+            }]
+          }
         }
-      }
-    });
+      });
+    }
   });
 }
 
-function clearStorage() {
-  localStorage.clear();
-  remove();
-}
-
 function remove() {
+  if(chart != null) {
     chart.destroy();
-    getGraph();
+  }
 }
 
 function reactToGraph(reactButton) {
@@ -86,66 +87,82 @@ function reactToGraph(reactButton) {
   var countConfused = document.getElementById("num-react-confused");
   
   var data;
-  var docRef = db.collection("graphs").doc(graphID).get().then((doc) => {
-    if (doc.exists) {
-      data = doc.data();
-    
-      var numReact = [data.reactHappy, data.reactSad, data.reactAngry, data.reactLaugh, data.reactConfused];
-      console.log(numReact);
-      switch(reaction) {
-        case "react-happy":
-          numReact[0] += 1;
-          buttons[0].classList.toggle("selected");
-          db.collection("graphs").doc(graphID).set({
-            reactHappy : numReact[0]
-          }, { merge: true });
-          break;
-        case "react-sad":
-          numReact[1] += 1;
-          buttons[1].classList.toggle("selected");
-          db.collection("graphs").doc(graphID).set({
-            reactSad   : numReact[1]
-          }, { merge: true });
-          break;
-        case "react-angry":
-          numReact[2] += 1;
-          buttons[2].classList.toggle("selected");
-          db.collection("graphs").doc(graphID).set({
-            reactAngry : numReact[2]
-          }, { merge: true });
-          break;
-        case "react-laugh":
-          numReact[3] += 1;
-          buttons[3].classList.toggle("selected");
-          db.collection("graphs").doc(graphID).set({
-            reactLaugh : numReact[3]
-          }, { merge: true });
-          break;
-        case "react-confused":
-          numReact[4] += 1;
-          buttons[4].classList.toggle("selected");
-          db.collection("graphs").doc(graphID).set({
-            reactConfused : numReact[4]
-          }, { merge: true });
-          break;
-      }
-      countHappy.innerHTML = numReact[0];
-      countSad.innerHTML = numReact[1];
-      countAngry.innerHTML = numReact[2];
-      countLaugh.innerHTML = numReact[3];
-      countConfused.innerHTML = numReact[4];
-
-      buttons.forEach((element) => {element.disabled = true});
-      document.getElementById("nextGraph").disabled = false;
+  if(graphID == "New Graph") {
+    exportGraph(graph.data);
+  } else {
+    var docRef = db.collection("graphs").doc(graphID).get().then((doc) => {
+      if (doc.exists) {
+        data = doc.data();
       
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
-  });
+        var numReact = [data.reactHappy, data.reactSad, data.reactAngry, data.reactLaugh, data.reactConfused];
+        switch(reaction) {
+          case "react-happy":
+            numReact[0] += 1;
+            buttons[0].classList.toggle("selected");
+            db.collection("graphs").doc(graphID).set({
+              reactHappy : numReact[0]
+            }, { merge: true });
+            break;
+          case "react-sad":
+            numReact[1] += 1;
+            buttons[1].classList.toggle("selected");
+            db.collection("graphs").doc(graphID).set({
+              reactSad   : numReact[1]
+            }, { merge: true });
+            break;
+          case "react-angry":
+            numReact[2] += 1;
+            buttons[2].classList.toggle("selected");
+            db.collection("graphs").doc(graphID).set({
+              reactAngry : numReact[2]
+            }, { merge: true });
+            break;
+          case "react-laugh":
+            numReact[3] += 1;
+            buttons[3].classList.toggle("selected");
+            db.collection("graphs").doc(graphID).set({
+              reactLaugh : numReact[3]
+            }, { merge: true });
+            break;
+          case "react-confused":
+            numReact[4] += 1;
+            buttons[4].classList.toggle("selected");
+            db.collection("graphs").doc(graphID).set({
+              reactConfused : numReact[4]
+            }, { merge: true });
+            break;
+        }
+        countHappy.innerHTML = numReact[0];
+        countSad.innerHTML = numReact[1];
+        countAngry.innerHTML = numReact[2];
+        countLaugh.innerHTML = numReact[3];
+        countConfused.innerHTML = numReact[4];
+
+        disableReactions();
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    });
+  }
 }
 
 function nextGraph() {
+  enableReactions();
+  var graphID = localStorage.getItem("currentGraph");
+  localStorage.setItem(graphID, true);
+  remove();
+  getGraph();
+}
+
+function disableReactions() {
+  var buttons = [document.getElementById("react-happy"), document.getElementById("react-sad"), document.getElementById("react-angry"), document.getElementById("react-laugh"), document.getElementById("react-confused")];
+  buttons.forEach((element) => {element.disabled = true});
+  document.getElementById("nextGraph").disabled = false;
+  document.getElementById("createGraph").disabled = false;
+}
+
+function enableReactions() {
   var buttons = [document.getElementById("react-happy"), 
                 document.getElementById("react-sad"),
                 document.getElementById("react-angry"),
@@ -169,13 +186,5 @@ function nextGraph() {
     element.classList.remove("selected");
   });
   document.getElementById("nextGraph").disabled = true;
-  var graphID = localStorage.getItem("currentGraph");
-  localStorage.setItem(graphID, true);
-  remove();
-}
-
-function report() {
-  id = localStorage.getItem("currentGraph");
-  console.log(id);
-  // remove();
+  document.getElementById("createGraph").disabled = true;
 }
